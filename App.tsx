d5583a31b939ddf28130a4df509b9a06b91ea77a
Laís Rhianne <lays.rhianne@gmail.com>
2021-09-19 16:19:38 -0300

    dashboard refactored to use styled components

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
@@ -8,12 +6,3 @@ export default function App() {
     <Dashboard />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
