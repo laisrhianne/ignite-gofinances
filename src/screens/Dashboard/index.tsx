ffb58ab8476181e864aaf8213cd1ccdcdeb8035a
Laís Rhianne <lays.rhianne@gmail.com>
2021-09-19 21:16:50 -0300

    Icon added

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import { 
   Container,
@@ -9,7 +8,8 @@ import {
   Photo,
   User,
   UserGreeting,
-  UserName
+  UserName,
+  Icon
  } from './styles';
 
 export function Dashboard() {
@@ -24,6 +24,7 @@ export function Dashboard() {
               <UserName>Laís</UserName>
             </User>
           </UserInfo>
+          <Icon name="power" />
         </UserWrapper>
       </Header>
     </Container>
