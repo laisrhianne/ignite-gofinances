9ce211a175aaf03d016da3e50eff435a379f8ed4
Laís Rhianne <lays.rhianne@gmail.com>
2021-09-19 21:08:38 -0300

    header created

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Text } from 'react-native';
 
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+ } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/11474951?v=4'}} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Laís</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
